8bf244201507927b5b259a59c14b3e1407962448
ReZero <ghostofpast@icloud.com>
2023-04-12 13:39:00 +0300

    add pj005

---
+++ project005_Square/Program.cs
@@ -0,0 +1,57 @@
+﻿//Программа принимает на вход два числа и проверяет, является ли одно число квадратом второго.
+
+Main();
+
+void Main()
+{
+    bool isWorking = true;
+
+    while (isWorking)
+    {
+        Console.Write("Input task: ");
+        string task = Console.ReadLine();
+
+        switch(task)
+        {
+            case "t1": Task1(); break;
+            case "exit": isWorking = false; break;
+            default:
+            break;
+        }
+    }
+}
+
+void Task1()
+{
+    int first = Readint("a");
+    int second = Readint("b");
+
+    int result1 = first * first;
+    int result2 = second * second;
+
+    if (result1 == second)
+    {
+        Console.WriteLine("Число " + second + " является квадратом числа " + first);
+    }
+    else if (result2 == first)
+    {
+        Console.WriteLine("Число " + first + " является квадратом числа " + second);
+    }
+    else
+    {
+        Console.WriteLine("Ни одно из чисел не является квадратом второго");
+    }
+}
+
+int Readint(string argument)
+{
+    int number;
+
+    Console.Write($"Input {argument}:");
+
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It's not a number!");
+    }
+    return number;
+}
